b56140b04f78727171ed118e5833c1e4f781c11c
Alex Saskevich <alexhomel@ya.ru>
2015-03-28 15:56:38 +0300

    Do not create new user after refresh

--- public/mobile/app.js
+++ public/mobile/app.js
@@ -18,12 +18,11 @@ socket.on('connectedUsers', function(msg) {
 socket.emit('connectedUsers', {});
 
 var id = '';
+var color = '';
 var balls = [];
 var username = '';
 
 $(function() {
-    id = '' + Math.floor(Math.random() * 254);
-    color = Math.floor(Math.random() * 0xffffff);
     username = '';
 
     $('.ready-btn').click(function() {
@@ -40,59 +39,73 @@ $(function() {
             return;
         }
         $('.login').remove();
+        if (!localStorage[username]) {
+            console.log('New user');
+            id = '' + Math.floor(Math.random() * 254);
+            color = Math.floor(Math.random() * 0xffffff);
+            localStorage[username] = JSON.stringify({
+                id: id,
+                color: color
+            });
+        } else {
+            console.log('Use existing user');
+            id = JSON.parse(localStorage[username]).id;
+            color = JSON.parse(localStorage[username]).color;
+        }
+        console.log(id);
+
+        socket.emit('updateMessage', {
+            id: id,
+            x: 1,
+            y: 0,
+            z: 1,
+            color: color,
+            username: username
+        });
+        window.addEventListener("devicemotion", handleMotionEvent, true);
+
+        function handleMotionEvent(event) {
+            var x = event.accelerationIncludingGravity.x;
+            var y = event.accelerationIncludingGravity.y;
+            var z = (event.accelerationIncludingGravity.z);
+            if (Math.abs(x) > 10) x = 10 * Math.sign(x);
+            if (Math.abs(y) > 5) y = 5 * Math.sign(y);
+            //if (Math.abs(z) > 5) z = 5 * Math.sign(z);
+            if (x) $('.x').html(x.toFixed(0));
+            if (y) $('.y').html(y.toFixed(0));
+            if (z) $('.z').html(z.toFixed(0));
+            if (id != "")
+                sendXYZ(x, y, z);
+            else {
+                color = Math.floor(Math.random() * 0xffffff);
+                balls[id] = addSphere(scene, 1, 0, 1, 0, color);
+                socket.emit('updateMessage', {
+                    id: id,
+                    x: 1,
+                    y: 0,
+                    z: 1,
+                    color: color,
+                    username: username
+                });
+            }
+        }
+
+
+        function sendXYZ(x, y, z) {
+            console.log(id);
+            socket.emit('updateMessage', {
+                x: x,
+                y: y,
+                z: z,
+                id: id,
+                color: color,
+                username: username
+            });
+        }
     });
 
 
     $(".color-mobile").css({
         "background": "#" + color.toString(16)
     });
-
-    socket.emit('updateMessage', {
-        id: id,
-        x: 1,
-        y: 0,
-        z: 1,
-        color: color,
-        username: username
-    });
-    window.addEventListener("devicemotion", handleMotionEvent, true);
-
-    function handleMotionEvent(event) {
-        var x = event.accelerationIncludingGravity.x;
-        var y = event.accelerationIncludingGravity.y;
-        var z = (event.accelerationIncludingGravity.z);
-        if (Math.abs(x) > 10) x = 10 * Math.sign(x);
-        if (Math.abs(y) > 5) y = 5 * Math.sign(y);
-        //if (Math.abs(z) > 5) z = 5 * Math.sign(z);
-        if (x) $('.x').html(x.toFixed(0));
-        if (y) $('.y').html(y.toFixed(0));
-        if (z) $('.z').html(z.toFixed(0));
-        if (id != "")
-            sendXYZ(x, y, z);
-        else {
-            color = Math.floor(Math.random() * 0xffffff);
-            balls[id] = addSphere(scene, 1, 0, 1, 0, color);
-            socket.emit('updateMessage', {
-                id: id,
-                x: 1,
-                y: 0,
-                z: 1,
-                color: color,
-                username: username
-            });
-        }
-    }
-
-
-    function sendXYZ(x, y, z) {
-        console.log(id);
-        socket.emit('updateMessage', {
-            x: x,
-            y: y,
-            z: z,
-            id: id,
-            color: color,
-            username: username
-        });
-    }
 });
