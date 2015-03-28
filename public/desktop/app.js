dd0248a56fb033260accac3b61d058fc81926140
Danik Tsyrkunov <danik.cyrkunov@gmail.com>
2015-03-28 16:16:45 +0300

    Merge branch 'master' of https://github.com/ZeFort/wth
    Conflicts:
    	app.jsttgvtgvtgv5tgtg5tgt5g5tg
    	views/mobile.html

--- public/desktop/app.js
+++ public/desktop/app.js
@@ -1,4 +1,4 @@
-var socket = io.connect('10.168.0.115:3010');
+var socket = io.connect('10.168.1.29:3010');
 
 var balls = [];
 var readyPlayerCount = 0;
@@ -11,7 +11,7 @@ var R = 4;
 $(function() {
     $('.elapsed-players').html(needToStart + '');
     var addPlane = function(scene, w, h, t, x, y, z, col) {
-        var planeGeometry = new THREE.CubeGeometry(w, h, t, 10);
+        var planeGeometry = new THREE.BoxGeometry(w, h, t, 10);
         var planeMaterial = new THREE.MeshLambertMaterial({
             color: col || 0xffffff
         });
@@ -42,13 +42,14 @@ $(function() {
     animate();
 
     function init() {
+        balls = [];
         scene = new THREE.Scene();
         camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
         renderer = new THREE.WebGLRenderer();
         renderer.setSize(window.innerWidth, window.innerHeight);
         renderer.shadowMapEnabled = true;
 
-        generateTiles(2000);
+        generateTiles(12);
 
         // position and point the camera to the center of the scene
         updateCameraPosition(camera, 40);
@@ -77,7 +78,7 @@ $(function() {
     function render() {
         var longestX = getFirstPlayerPosition();
 
-        //handleCollisions();
+
         handleCrossingTheLine();
         updateCameraPosition(camera, longestX + 40);
 
@@ -101,31 +102,7 @@ $(function() {
         return longestX;
     }
 
-    function handleCollisions(){
-        console.log(balls);
-        for(var key in balls){
-            if(balls.hasOwnProperty(key)){
-               for(var key2 in balls){
-                    if(balls.hasOwnProperty(key2) && key !== key2){
-                        var s1 = balls[key];
-                        var s2 = balls[key2];
 
-                        if(getPythogarExpression(s1.position.x, s2.position.x, s1.position.z, s2.position.z)){
-                            swapSpeeds(s1, s2);
-                        }
-                    }
-               }
-            } 
-        }
-    }
-
-    function getPythogarExpression(x1, x2, y1, y2) {
-        return Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) <= 4 * R * R;
-    }
-
-    function swapSpeeds(sph1, sph2) {
-        //write swap speeds logic here
-    }
 
     function handleCrossingTheLine() {
         for(var key in balls){
@@ -165,7 +142,7 @@ $(function() {
 
     id = "" + Math.floor(Math.random() * 254);
     color = Math.floor(Math.random() * 0xffffff);
-    balls[id] = addSphere(scene, R, 0, 5, 0, color);
+    balls[id] = addSphere(scene, R, 100, 5, 0, color);
     socket.emit('updateMessage', {
         id: id,
         x: 1,
@@ -182,11 +159,40 @@ $(function() {
         var x = msg.x;
         var y = msg.y;
         if (!msg.id || msg.id === '') return;
-        console.log('---', msg);
         if (!gameStarted) return;
-        balls[msg.id].position.x += -(Math.abs(Math.abs(x)-10)) / 15;
-        balls[msg.id].position.z += (-y) / 10;
+        handleCollisions(msg.id, x, y);
     });
+
+    function handleCollisions(id, x, y){
+        for(var key in balls){
+            if(balls.hasOwnProperty(key) && (id !== key)){
+                var sphere1 = balls[id],
+                    sphere2 = balls[key];
+                if (getPythogarExpression(sphere1.position.x, sphere2.position.x, sphere1.position.z, sphere2.position.z)){
+                    if (sphere1.position.z > sphere2.position.z && y > 0 || sphere1.position.z < sphere2.position.z && y < 0 ){
+                        balls[id].position.x += -(Math.abs(Math.abs(x)-10)) / 15;
+                        balls[id].position.z += 0;
+                    }else{
+                        balls[id].position.x += -(Math.abs(Math.abs(x)-10)) / 15;
+                        balls[id].position.z += (-y) / 10;
+                    }
+                }
+                else {
+                    balls[id].position.x += -(Math.abs(Math.abs(x)-10)) / 15;
+                    balls[id].position.z += (-y) / 10;
+                }
+
+            }
+        }
+    }
+
+    function getPythogarExpression(x1, x2, y1, y2) {
+        return Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2) <= 4 * R * R;
+    }
+
+    function swapSpeeds(sph1, sph2) {
+        //write swap speeds logic here
+    }
 
     socket.on('initClientMessage', function(msg) {
         console.log(msg);
